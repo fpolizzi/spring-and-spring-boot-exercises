51c735dd61aa7d010fea7bf5b3cbfe3fafa08f17
Franco Polizzi <me@fpolizzi.com>
2026-04-28 01:21:18 +0200

    feat: annotate class as component

--- src/main/java/com/amigoscode/greeting/GreetingService.java
+++ src/main/java/com/amigoscode/greeting/GreetingService.java
@@ -1,11 +1,14 @@
 package com.amigoscode.greeting;
 
-// TODO: 01 - Add @Component annotation to this class so Spring manages it as a bean
+// DONE: 01 - Add @Component annotation to this class so Spring manages it as a bean
 
 // TODO: 03 - Add @Qualifier("greetingMessage") to the constructor parameter
 
 // TODO: 04 - Add @PostConstruct and @PreDestroy lifecycle hooks
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class GreetingService {
 
     private final String greeting;
