8bf5ba5a88773f78de77bb98474696cc4d0d4239
Franco Polizzi <me@fpolizzi.com>
2026-04-28 15:54:18 +0200

    feat: solve todo 5

--- src/main/java/com/amigoscode/user/UserService.java
+++ src/main/java/com/amigoscode/user/UserService.java
@@ -1,12 +1,21 @@
 package com.amigoscode.user;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
-// TODO: 05 - Add @Service annotation to this class
+// DONE: 05 - Add @Service annotation to this class
 //  Inject UserRepository via constructor injection
 //  Use the injected repository in getAllUsers()
 
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public List<String> getAllUsers() {
         return List.of(); // Replace with userRepository.findAllUsers()
