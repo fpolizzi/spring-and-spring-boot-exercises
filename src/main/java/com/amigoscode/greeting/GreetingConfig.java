6cee1d0fa90b7c4eba0288b3a1a8a65236c64bdd
Franco Polizzi <me@fpolizzi.com>
2026-04-28 02:11:04 +0200

    feat: solve todo 3

--- src/main/java/com/amigoscode/greeting/GreetingConfig.java
+++ src/main/java/com/amigoscode/greeting/GreetingConfig.java
@@ -1,12 +1,18 @@
 package com.amigoscode.greeting;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GreetingConfig {
 
-    // TODO: 03 - Create a @Bean("greetingMessage") method that returns "Hello"
+    // DONE: 03 - Create a @Bean("greetingMessage") method that returns "Hello"
     //  Add the @Bean annotation with the name "greetingMessage"
     //  The method should return a String with the value "Hello"
 
+    @Bean("greetingMessage")
+    public String greetingMessage() {
+
+        return "Hello";
+    }
 }
--- src/main/java/com/amigoscode/greeting/GreetingService.java
+++ src/main/java/com/amigoscode/greeting/GreetingService.java
@@ -2,10 +2,11 @@ package com.amigoscode.greeting;
 
 // DONE: 01 - Add @Component annotation to this class so Spring manages it as a bean
 
-// TODO: 03 - Add @Qualifier("greetingMessage") to the constructor parameter
+// DONE: 03 - Add @Qualifier("greetingMessage") to the constructor parameter
 
 // TODO: 04 - Add @PostConstruct and @PreDestroy lifecycle hooks
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +14,7 @@ public class GreetingService {
 
     private final String greeting;
 
-    public GreetingService(String greeting) {
+    public GreetingService(@Qualifier("greetingMessage") String greeting) {
         this.greeting = greeting;
     }
 
